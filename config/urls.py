035e73ce827e3cc21abb38c81cae68f50faa05ed
Diego Rubio Canales <diegorubiocanales2006@gmail.com>
2026-04-03 17:39:54 +0200

    static files: add media

--- config/urls.py
+++ config/urls.py
@@ -10,7 +10,4 @@ urlpatterns = [
     path('accounts/', include('apps.accounts.urls')),
     path('blog/', include('apps.blog.urls')),
     path('forum/', include('apps.forum.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
