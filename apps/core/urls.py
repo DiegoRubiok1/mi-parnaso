7e0d79b8f704aea785e0ebe8da4fb512574d6adf
Diego Rubio Canales <diegorubiocanales2006@gmail.com>
2026-04-05 15:26:14 +0200

    feat: terms and conditions of the forum

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, terms_conditions_view
 
 app_name = "core"
 
 urlpatterns = [
     path("", home_view, name="home"),
+    path("terminos-y-condiciones/", terms_conditions_view, name="terms-and-conditions"),
 ]
-
--- apps/core/views.py
+++ apps/core/views.py
@@ -7,3 +7,6 @@ def home_view(request):
 	latest_posts = Post.objects.filter(status="published").order_by("-published_at")[:5]
 	context = {"latest_posts": latest_posts}
 	return render(request, "core/home.html", context)
+
+def terms_conditions_view(request):
+    return render(request, "core/terms_and_conditions.html")
